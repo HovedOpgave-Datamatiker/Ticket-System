6a2b5bc75e7c290f654f9bfcb150155943b23457
philipbenn <phil658c@stud.kea.dk>
2024-11-27 10:08:25 +0100

    Add project files.

---
+++ Hovedopgave/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hovedopgave.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
